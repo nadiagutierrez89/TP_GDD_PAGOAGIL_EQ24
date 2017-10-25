4181dd4fc0bc1520ee4a15cdbea19098497b7fd6
Lucia <lucyutn@gmail.com>
2017-10-25 20:16:42 -0300

    odio github vol 6
    correccion  de problema de abm rol
    
    limpieza solucion.

--- PagoAgilFrba/AbmRol/ABMRol.Designer.cs
+++ PagoAgilFrba/AbmRol/ABMRol.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PagoAgilFrba.AbmRol
 {
-    partial class Form1
+    partial class ABMRol
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ABMRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "ABMRol";
+            this.Text = "ABM Rol";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
