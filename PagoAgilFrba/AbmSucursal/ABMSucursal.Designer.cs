c94e5ad375581ab9ba1c0679ae6b53b4ed18c1d6
Lucia <lucyutn@gmail.com>
2017-10-25 18:10:58 -0300

    odio github vol 4(?)  pantallas varias
    ABM Sucursal
    Listado estadistico (hay que definir en grupo unas cositas de esta pantalla)

---
+++ PagoAgilFrba/AbmSucursal/ABMSucursal.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PagoAgilFrba.AbmSucursal
+{
+    partial class ABMSucursal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.abmsucursal_lab_nombre = new System.Windows.Forms.Label();
+            this.ambsucursal_lab_direccion = new System.Windows.Forms.Label();
+            this.abmsucursal_lab_cp = new System.Windows.Forms.Label();
+            this.abmsucursal_tb_nombre = new System.Windows.Forms.TextBox();
+            this.abmsucursal_tb_direccion = new System.Windows.Forms.TextBox();
+            this.abmsucursal_tb_cp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // abmsucursal_lab_nombre
+            // 
+            this.abmsucursal_lab_nombre.AutoSize = true;
+            this.abmsucursal_lab_nombre.Location = new System.Drawing.Point(22, 26);
+            this.abmsucursal_lab_nombre.Name = "abmsucursal_lab_nombre";
+            this.abmsucursal_lab_nombre.Size = new System.Drawing.Size(91, 13);
+            this.abmsucursal_lab_nombre.TabIndex = 0;
+            this.abmsucursal_lab_nombre.Text = "Nombre Sucursal:";
+            // 
+            // ambsucursal_lab_direccion
+            // 
+            this.ambsucursal_lab_direccion.AutoSize = true;
+            this.ambsucursal_lab_direccion.Location = new System.Drawing.Point(22, 73);
+            this.ambsucursal_lab_direccion.Name = "ambsucursal_lab_direccion";
+            this.ambsucursal_lab_direccion.Size = new System.Drawing.Size(55, 13);
+            this.ambsucursal_lab_direccion.TabIndex = 1;
+            this.ambsucursal_lab_direccion.Text = "Dirección:";
+            this.ambsucursal_lab_direccion.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // abmsucursal_lab_cp
+            // 
+            this.abmsucursal_lab_cp.AutoSize = true;
+            this.abmsucursal_lab_cp.Location = new System.Drawing.Point(22, 131);
+            this.abmsucursal_lab_cp.Name = "abmsucursal_lab_cp";
+            this.abmsucursal_lab_cp.Size = new System.Drawing.Size(75, 13);
+            this.abmsucursal_lab_cp.TabIndex = 2;
+            this.abmsucursal_lab_cp.Text = "Código Postal:";
+            // 
+            // abmsucursal_tb_nombre
+            // 
+            this.abmsucursal_tb_nombre.Location = new System.Drawing.Point(25, 43);
+            this.abmsucursal_tb_nombre.Name = "abmsucursal_tb_nombre";
+            this.abmsucursal_tb_nombre.Size = new System.Drawing.Size(247, 20);
+            this.abmsucursal_tb_nombre.TabIndex = 3;
+            // 
+            // abmsucursal_tb_direccion
+            // 
+            this.abmsucursal_tb_direccion.Location = new System.Drawing.Point(25, 92);
+            this.abmsucursal_tb_direccion.Name = "abmsucursal_tb_direccion";
+            this.abmsucursal_tb_direccion.Size = new System.Drawing.Size(247, 20);
+            this.abmsucursal_tb_direccion.TabIndex = 4;
+            // 
+            // abmsucursal_tb_cp
+            // 
+            this.abmsucursal_tb_cp.Location = new System.Drawing.Point(25, 148);
+            this.abmsucursal_tb_cp.Name = "abmsucursal_tb_cp";
+            this.abmsucursal_tb_cp.Size = new System.Drawing.Size(100, 20);
+            this.abmsucursal_tb_cp.TabIndex = 5;
+            // 
+            // ABMSucursal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.abmsucursal_tb_cp);
+            this.Controls.Add(this.abmsucursal_tb_direccion);
+            this.Controls.Add(this.abmsucursal_tb_nombre);
+            this.Controls.Add(this.abmsucursal_lab_cp);
+            this.Controls.Add(this.ambsucursal_lab_direccion);
+            this.Controls.Add(this.abmsucursal_lab_nombre);
+            this.Name = "ABMSucursal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "ABM Sucursal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label abmsucursal_lab_nombre;
+        private System.Windows.Forms.Label ambsucursal_lab_direccion;
+        private System.Windows.Forms.Label abmsucursal_lab_cp;
+        private System.Windows.Forms.TextBox abmsucursal_tb_nombre;
+        private System.Windows.Forms.TextBox abmsucursal_tb_direccion;
+        private System.Windows.Forms.TextBox abmsucursal_tb_cp;
+    }
+}
---
+++ PagoAgilFrba/ListadoEstadistico/Listado Estadistico.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace PagoAgilFrba.ListadoEstadistico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.estadistica_but_FacXEmpresa = new System.Windows.Forms.Button();
+            this.estadistica_but_EmpresaMontoRendido = new System.Windows.Forms.Button();
+            this.estadistica_but_clientesPagos_ = new System.Windows.Forms.Button();
+            this.estadistica_but_clientesCumplidores = new System.Windows.Forms.Button();
+            this.estadistica_lab_FacXEmpresa = new System.Windows.Forms.Label();
+            this.estadistica_cbox_trimFacXEmpresa = new System.Windows.Forms.ComboBox();
+            this.estadisticas_cbox_trimEmpresaMayorMonto = new System.Windows.Forms.ComboBox();
+            this.estadistica_lab_EmpresaMontoRendido = new System.Windows.Forms.Label();
+            this.estadistica_lab_clientesPagos = new System.Windows.Forms.Label();
+            this.estadistica_lab_clientesCumplidores = new System.Windows.Forms.Label();
+            this.estadisticas_cbox_trimClientesPagos = new System.Windows.Forms.ComboBox();
+            this.estadisticas_cbox_trimClientesCumplidores = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // estadistica_but_FacXEmpresa
+            // 
+            this.estadistica_but_FacXEmpresa.Location = new System.Drawing.Point(197, 30);
+            this.estadistica_but_FacXEmpresa.Name = "estadistica_but_FacXEmpresa";
+            this.estadistica_but_FacXEmpresa.Size = new System.Drawing.Size(75, 23);
+            this.estadistica_but_FacXEmpresa.TabIndex = 0;
+            this.estadistica_but_FacXEmpresa.Text = "Consultar";
+            this.estadistica_but_FacXEmpresa.UseVisualStyleBackColor = true;
+            // 
+            // estadistica_but_EmpresaMontoRendido
+            // 
+            this.estadistica_but_EmpresaMontoRendido.Location = new System.Drawing.Point(197, 78);
+            this.estadistica_but_EmpresaMontoRendido.Name = "estadistica_but_EmpresaMontoRendido";
+            this.estadistica_but_EmpresaMontoRendido.Size = new System.Drawing.Size(75, 23);
+            this.estadistica_but_EmpresaMontoRendido.TabIndex = 1;
+            this.estadistica_but_EmpresaMontoRendido.Text = "Consultar";
+            this.estadistica_but_EmpresaMontoRendido.UseVisualStyleBackColor = true;
+            // 
+            // estadistica_but_clientesPagos_
+            // 
+            this.estadistica_but_clientesPagos_.Location = new System.Drawing.Point(197, 131);
+            this.estadistica_but_clientesPagos_.Name = "estadistica_but_clientesPagos_";
+            this.estadistica_but_clientesPagos_.Size = new System.Drawing.Size(75, 23);
+            this.estadistica_but_clientesPagos_.TabIndex = 2;
+            this.estadistica_but_clientesPagos_.Text = "Consultar";
+            this.estadistica_but_clientesPagos_.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.estadistica_but_clientesPagos_.UseVisualStyleBackColor = true;
+            // 
+            // estadistica_but_clientesCumplidores
+            // 
+            this.estadistica_but_clientesCumplidores.Location = new System.Drawing.Point(197, 180);
+            this.estadistica_but_clientesCumplidores.Name = "estadistica_but_clientesCumplidores";
+            this.estadistica_but_clientesCumplidores.Size = new System.Drawing.Size(75, 23);
+            this.estadistica_but_clientesCumplidores.TabIndex = 3;
+            this.estadistica_but_clientesCumplidores.Text = "Consultar";
+            this.estadistica_but_clientesCumplidores.UseVisualStyleBackColor = true;
+            // 
+            // estadistica_lab_FacXEmpresa
+            // 
+            this.estadistica_lab_FacXEmpresa.AutoSize = true;
+            this.estadistica_lab_FacXEmpresa.Location = new System.Drawing.Point(13, 13);
+            this.estadistica_lab_FacXEmpresa.Name = "estadistica_lab_FacXEmpresa";
+            this.estadistica_lab_FacXEmpresa.Size = new System.Drawing.Size(225, 13);
+            this.estadistica_lab_FacXEmpresa.TabIndex = 4;
+            this.estadistica_lab_FacXEmpresa.Text = "Porcentaje de facturas cobradas por empresa:";
+            // 
+            // estadistica_cbox_trimFacXEmpresa
+            // 
+            this.estadistica_cbox_trimFacXEmpresa.FormattingEnabled = true;
+            this.estadistica_cbox_trimFacXEmpresa.Location = new System.Drawing.Point(16, 32);
+            this.estadistica_cbox_trimFacXEmpresa.Name = "estadistica_cbox_trimFacXEmpresa";
+            this.estadistica_cbox_trimFacXEmpresa.Size = new System.Drawing.Size(121, 21);
+            this.estadistica_cbox_trimFacXEmpresa.TabIndex = 5;
+            // 
+            // estadisticas_cbox_trimEmpresaMayorMonto
+            // 
+            this.estadisticas_cbox_trimEmpresaMayorMonto.FormattingEnabled = true;
+            this.estadisticas_cbox_trimEmpresaMayorMonto.Location = new System.Drawing.Point(16, 80);
+            this.estadisticas_cbox_trimEmpresaMayorMonto.Name = "estadisticas_cbox_trimEmpresaMayorMonto";
+            this.estadisticas_cbox_trimEmpresaMayorMonto.Size = new System.Drawing.Size(121, 21);
+            this.estadisticas_cbox_trimEmpresaMayorMonto.TabIndex = 6;
+            // 
+            // estadistica_lab_EmpresaMontoRendido
+            // 
+            this.estadistica_lab_EmpresaMontoRendido.AutoSize = true;
+            this.estadistica_lab_EmpresaMontoRendido.Location = new System.Drawing.Point(16, 62);
+            this.estadistica_lab_EmpresaMontoRendido.Name = "estadistica_lab_EmpresaMontoRendido";
+            this.estadistica_lab_EmpresaMontoRendido.Size = new System.Drawing.Size(178, 13);
+            this.estadistica_lab_EmpresaMontoRendido.TabIndex = 7;
+            this.estadistica_lab_EmpresaMontoRendido.Text = "Empresas con mayor monto rendido:";
+            // 
+            // estadistica_lab_clientesPagos
+            // 
+            this.estadistica_lab_clientesPagos.AutoSize = true;
+            this.estadistica_lab_clientesPagos.Location = new System.Drawing.Point(16, 114);
+            this.estadistica_lab_clientesPagos.Name = "estadistica_lab_clientesPagos";
+            this.estadistica_lab_clientesPagos.Size = new System.Drawing.Size(119, 13);
+            this.estadistica_lab_clientesPagos.TabIndex = 8;
+            this.estadistica_lab_clientesPagos.Text = "Clientes con mas pagos";
+            // 
+            // estadistica_lab_clientesCumplidores
+            // 
+            this.estadistica_lab_clientesCumplidores.AutoSize = true;
+            this.estadistica_lab_clientesCumplidores.Location = new System.Drawing.Point(16, 163);
+            this.estadistica_lab_clientesCumplidores.Name = "estadistica_lab_clientesCumplidores";
+            this.estadistica_lab_clientesCumplidores.Size = new System.Drawing.Size(249, 13);
+            this.estadistica_lab_clientesCumplidores.TabIndex = 9;
+            this.estadistica_lab_clientesCumplidores.Text = "Clientes con mayor porcentaje de facturas pagadas";
+            // 
+            // estadisticas_cbox_trimClientesPagos
+            // 
+            this.estadisticas_cbox_trimClientesPagos.FormattingEnabled = true;
+            this.estadisticas_cbox_trimClientesPagos.Location = new System.Drawing.Point(19, 131);
+            this.estadisticas_cbox_trimClientesPagos.Name = "estadisticas_cbox_trimClientesPagos";
+            this.estadisticas_cbox_trimClientesPagos.Size = new System.Drawing.Size(121, 21);
+            this.estadisticas_cbox_trimClientesPagos.TabIndex = 10;
+            // 
+            // estadisticas_cbox_trimClientesCumplidores
+            // 
+            this.estadisticas_cbox_trimClientesCumplidores.FormattingEnabled = true;
+            this.estadisticas_cbox_trimClientesCumplidores.Location = new System.Drawing.Point(16, 180);
+            this.estadisticas_cbox_trimClientesCumplidores.Name = "estadisticas_cbox_trimClientesCumplidores";
+            this.estadisticas_cbox_trimClientesCumplidores.Size = new System.Drawing.Size(121, 21);
+            this.estadisticas_cbox_trimClientesCumplidores.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.estadisticas_cbox_trimClientesCumplidores);
+            this.Controls.Add(this.estadisticas_cbox_trimClientesPagos);
+            this.Controls.Add(this.estadistica_lab_clientesCumplidores);
+            this.Controls.Add(this.estadistica_lab_clientesPagos);
+            this.Controls.Add(this.estadistica_lab_EmpresaMontoRendido);
+            this.Controls.Add(this.estadisticas_cbox_trimEmpresaMayorMonto);
+            this.Controls.Add(this.estadistica_cbox_trimFacXEmpresa);
+            this.Controls.Add(this.estadistica_lab_FacXEmpresa);
+            this.Controls.Add(this.estadistica_but_clientesCumplidores);
+            this.Controls.Add(this.estadistica_but_clientesPagos_);
+            this.Controls.Add(this.estadistica_but_EmpresaMontoRendido);
+            this.Controls.Add(this.estadistica_but_FacXEmpresa);
+            this.Name = "Form1";
+            this.Text = "Listado Estadístico ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button estadistica_but_FacXEmpresa;
+        private System.Windows.Forms.Button estadistica_but_EmpresaMontoRendido;
+        private System.Windows.Forms.Button estadistica_but_clientesPagos_;
+        private System.Windows.Forms.Button estadistica_but_clientesCumplidores;
+        private System.Windows.Forms.Label estadistica_lab_FacXEmpresa;
+        private System.Windows.Forms.ComboBox estadistica_cbox_trimFacXEmpresa;
+        private System.Windows.Forms.ComboBox estadisticas_cbox_trimEmpresaMayorMonto;
+        private System.Windows.Forms.Label estadistica_lab_EmpresaMontoRendido;
+        private System.Windows.Forms.Label estadistica_lab_clientesPagos;
+        private System.Windows.Forms.Label estadistica_lab_clientesCumplidores;
+        private System.Windows.Forms.ComboBox estadisticas_cbox_trimClientesPagos;
+        private System.Windows.Forms.ComboBox estadisticas_cbox_trimClientesCumplidores;
+    }
+}
