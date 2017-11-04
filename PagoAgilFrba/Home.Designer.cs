2124c98bc01a72bbe791dfe9e6d8beee6b69d3e8
Nadia <nadiagutierrez89@yahoo.com.ar>
2017-11-03 21:23:12 -0300

    cambio diseño de home

--- PagoAgilFrba/Home.Designer.cs
+++ PagoAgilFrba/Home.Designer.cs
@@ -42,90 +42,90 @@
             // 
             // home_but_abmcliente
             // 
-            this.home_but_abmcliente.Location = new System.Drawing.Point(13, 23);
+            this.home_but_abmcliente.Location = new System.Drawing.Point(49, 20);
             this.home_but_abmcliente.Name = "home_but_abmcliente";
-            this.home_but_abmcliente.Size = new System.Drawing.Size(75, 23);
+            this.home_but_abmcliente.Size = new System.Drawing.Size(108, 36);
             this.home_but_abmcliente.TabIndex = 0;
             this.home_but_abmcliente.Text = "Clientes";
             this.home_but_abmcliente.UseVisualStyleBackColor = true;
             // 
             // home_but_abmempresa
             // 
-            this.home_but_abmempresa.Location = new System.Drawing.Point(108, 22);
+            this.home_but_abmempresa.Location = new System.Drawing.Point(188, 20);
             this.home_but_abmempresa.Name = "home_but_abmempresa";
-            this.home_but_abmempresa.Size = new System.Drawing.Size(85, 23);
+            this.home_but_abmempresa.Size = new System.Drawing.Size(108, 36);
             this.home_but_abmempresa.TabIndex = 1;
             this.home_but_abmempresa.Text = "Empresas";
             this.home_but_abmempresa.UseVisualStyleBackColor = true;
             // 
             // home_but_abmfactura
             // 
-            this.home_but_abmfactura.Location = new System.Drawing.Point(13, 65);
+            this.home_but_abmfactura.Location = new System.Drawing.Point(49, 75);
             this.home_but_abmfactura.Name = "home_but_abmfactura";
-            this.home_but_abmfactura.Size = new System.Drawing.Size(88, 23);
+            this.home_but_abmfactura.Size = new System.Drawing.Size(107, 36);
             this.home_but_abmfactura.TabIndex = 2;
             this.home_but_abmfactura.Text = "ABM Factura";
             this.home_but_abmfactura.UseVisualStyleBackColor = true;
             // 
             // home_but_abmsucursal
             // 
-            this.home_but_abmsucursal.Location = new System.Drawing.Point(108, 64);
+            this.home_but_abmsucursal.Location = new System.Drawing.Point(188, 75);
             this.home_but_abmsucursal.Name = "home_but_abmsucursal";
-            this.home_but_abmsucursal.Size = new System.Drawing.Size(85, 23);
+            this.home_but_abmsucursal.Size = new System.Drawing.Size(108, 36);
             this.home_but_abmsucursal.TabIndex = 3;
             this.home_but_abmsucursal.Text = "Sucursales";
             this.home_but_abmsucursal.UseVisualStyleBackColor = true;
             // 
             // home_but_ambrol
             // 
-            this.home_but_ambrol.Location = new System.Drawing.Point(13, 107);
+            this.home_but_ambrol.Location = new System.Drawing.Point(188, 232);
             this.home_but_ambrol.Name = "home_but_ambrol";
-            this.home_but_ambrol.Size = new System.Drawing.Size(75, 23);
+            this.home_but_ambrol.Size = new System.Drawing.Size(107, 37);
             this.home_but_ambrol.TabIndex = 4;
             this.home_but_ambrol.Text = "Roles";
             this.home_but_ambrol.UseVisualStyleBackColor = true;
             // 
             // home_but_regusuario
             // 
-            this.home_but_regusuario.Location = new System.Drawing.Point(108, 106);
+            this.home_but_regusuario.Location = new System.Drawing.Point(50, 235);
             this.home_but_regusuario.Name = "home_but_regusuario";
-            this.home_but_regusuario.Size = new System.Drawing.Size(111, 23);
+            this.home_but_regusuario.Size = new System.Drawing.Size(106, 31);
             this.home_but_regusuario.TabIndex = 5;
             this.home_but_regusuario.Text = "Registrar Usuario";
             this.home_but_regusuario.UseVisualStyleBackColor = true;
             // 
             // home_but_regpagf
             // 
-            this.home_but_regpagf.Location = new System.Drawing.Point(12, 146);
+            this.home_but_regpagf.Location = new System.Drawing.Point(48, 128);
             this.home_but_regpagf.Name = "home_but_regpagf";
-            this.home_but_regpagf.Size = new System.Drawing.Size(142, 23);
+            this.home_but_regpagf.Size = new System.Drawing.Size(108, 38);
             this.home_but_regpagf.TabIndex = 6;
             this.home_but_regpagf.Text = "Registrar Pago de Factura";
             this.home_but_regpagf.UseVisualStyleBackColor = true;
             // 
             // home_but_rendirf
             // 
-            this.home_but_rendirf.Location = new System.Drawing.Point(13, 181);
+            this.home_but_rendirf.Location = new System.Drawing.Point(50, 182);
             this.home_but_rendirf.Name = "home_but_rendirf";
-            this.home_but_rendirf.Size = new System.Drawing.Size(97, 23);
+            this.home_but_rendirf.Size = new System.Drawing.Size(107, 37);
             this.home_but_rendirf.TabIndex = 7;
             this.home_but_rendirf.Text = "Rendir Facturas";
             this.home_but_rendirf.UseVisualStyleBackColor = true;
             // 
             // home_but_devolverf
             // 
-            this.home_but_devolverf.Location = new System.Drawing.Point(122, 181);
+            this.home_but_devolverf.Location = new System.Drawing.Point(187, 182);
             this.home_but_devolverf.Name = "home_but_devolverf";
-            this.home_but_devolverf.Size = new System.Drawing.Size(97, 23);
+            this.home_but_devolverf.Size = new System.Drawing.Size(108, 37);
             this.home_but_devolverf.TabIndex = 8;
             this.home_but_devolverf.Text = "Devolver Factura";
             this.home_but_devolverf.UseVisualStyleBackColor = true;
             // 
             // home_but_estadisticas
             // 
-            this.home_but_estadisticas.Location = new System.Drawing.Point(12, 215);
+            this.home_but_estadisticas.Location = new System.Drawing.Point(188, 129);
             this.home_but_estadisticas.Name = "home_but_estadisticas";
-            this.home_but_estadisticas.Size = new System.Drawing.Size(108, 23);
+            this.home_but_estadisticas.Size = new System.Drawing.Size(108, 37);
             this.home_but_estadisticas.TabIndex = 9;
             this.home_but_estadisticas.Text = "Listado Estadistico ";
             this.home_but_estadisticas.UseVisualStyleBackColor = true;
@@ -134,7 +134,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(350, 289);
             this.Controls.Add(this.home_but_estadisticas);
             this.Controls.Add(this.home_but_devolverf);
             this.Controls.Add(this.home_but_rendirf);
